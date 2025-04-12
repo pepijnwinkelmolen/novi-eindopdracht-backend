d0a5195eb258fe316e3b9d439dc2dd7c9f1a8045
pepijnwinkelmolen <pepijnwm1@hotmail.com>
2025-04-12 02:30:44 +0200

    feat: finished with the endpoints.

---
+++ src/test/java/com/demo/novieindopdracht/services/AdvertisementServiceTest.java
@@ -0,0 +1,106 @@
+package com.demo.novieindopdracht.services;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class AdvertisementServiceTest {
+
+    @InjectMocks
+    AdvertisementService advertisementService;
+
+    @Test
+    void getAllAdvertisementsLikeQuery() {
+        // Arrange
+
+
+        // Act
+
+
+        // Assert
+
+
+    }
+
+    @Test
+    void getAllAdvertisements() {
+        // Arrange
+
+
+        // Act
+
+
+        // Assert
+
+
+    }
+
+    @Test
+    void getAllAdvertisementsWithFilter() {
+        // Arrange
+
+
+        // Act
+
+
+        // Assert
+
+
+    }
+
+    @Test
+    void getAllAdvertisementsByCategory() {
+        // Arrange
+
+
+        // Act
+
+
+        // Assert
+
+
+    }
+
+    @Test
+    void getAdvertisementById() {
+        // Arrange
+
+
+        // Act
+
+
+        // Assert
+
+
+    }
+
+    @Test
+    void deleteAdvert() {
+        // Arrange
+
+
+        // Act
+
+
+        // Assert
+
+
+    }
+
+    @Test
+    void createAdvert() {
+        // Arrange
+
+
+        // Act
+
+
+        // Assert
+
+
+    }
+}
